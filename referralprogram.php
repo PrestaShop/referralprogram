861a1b17c8b49074e177f22ad2829850975a6f2c
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-09-08 09:15:52 +0200

    //Version update

--- referralprogram.php
+++ referralprogram.php
@@ -33,7 +33,7 @@ class ReferralProgram extends Module
 	{
 		$this->name = 'referralprogram';
 		$this->tab = 'advertising_marketing';
-		$this->version = '1.5.8';
+		$this->version = '1.5.9';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
